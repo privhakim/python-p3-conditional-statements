e430259d1fc2f118d1700857c0b90a4fa8e8e969
privhakim <abdihakim.hassan@student.moringaschool.com>
2023-08-17 21:14:11 +0300

    fixed errors on Taps and spaces

--- lib/control_flow.py
+++ lib/control_flow.py
@@ -1,48 +1,44 @@
-#!/usr/bin/env python3
-
 def admin_login(username, password):
-    # your code here
+    # Check if the username is "admin" or "ADMIN" and the password is "12345"
     if (username == "admin" or username == "ADMIN") and password == "12345":
         return "Access granted"
-    else: 
-         return "Access denied"
-    
+    else:
+        return "Access denied"
 
- def hows_the_weather(temperature):
-    if temperature <40:
-        return "its brisk out there!"
+def hows_the_weather(temperature):
+    # Check the temperature ranges and return appropriate responses
+    if temperature < 40:
+        return "It's brisk out there!"
     elif 40 <= temperature <= 65:
-        return "its a littly chilly out there"
-    elif temperature >85:
-        return "its too dang hot out there!"
-    else: 
-        return "its perfect out there!"
-    # your code here
-    
+        return "It's a little chilly out there!"
+    elif temperature > 85:
+        return "It's too dang hot out there!"
+    else:
+        return "It's perfect out there!"
 
- def fizzbuzz(num):
-    if num % 3 ==0 and num % 5 == 0:
-      return "FizzBuzz"
+def fizzbuzz(num):
+    # Check for multiples of 3, 5, or both, and return appropriate responses
+    if num % 3 == 0 and num % 5 == 0:
+        return "FizzBuzz"
     elif num % 3 == 0:
-      return "Fizz" 
-    elif num % 5 ==0: 
-      return "Buzz" 
-    else :
-      return num
+        return "Fizz"
+    elif num % 5 == 0:
+        return "Buzz"
+    else:
+        return num
 
-    
-
- def calculator(operation, num1, num2):
+def calculator(operation, num1, num2):
+    # Perform the operation based on the input and return the result
     if operation == "+":
-      return num1 +num2
+        return num1 + num2
     elif operation == "-":
-      return num1 -num2
+        return num1 - num2
     elif operation == "*":
-      return num1 * num2
+        return num1 * num2
     elif operation == "/":
-      if num2 != 0: # avoid division by zero
-          return num1 /num2
-      else: 
-          return "invalid operation! cannot divide by zero."
-    else: 
-      return "invalid operation!"
+        if num2 != 0:  # Avoid division by zero
+            return num1 / num2
+        else:
+            return "Invalid operation! Cannot divide by zero."
+    else:
+        return "Invalid operation!"
